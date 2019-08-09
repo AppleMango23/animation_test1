2d9410811f180ce465311ca8922facc5a40e2d6f
Noah Yek <ninjayek@gmail.com>
2019-08-09 18:24:12 +0800

    first commit

---
+++ lib/data.dart
@@ -0,0 +1,13 @@
+List<String> images = [
+  "assets/img1.png",
+  "assets/img2.png",
+  "assets/img3.png",
+  "assets/img4.png",
+];
+
+List<String> titles = [
+  "Haunted Ground",
+  "Fallen in love",
+  "The Dreaming lady",
+  "Jack the titan",
+];
